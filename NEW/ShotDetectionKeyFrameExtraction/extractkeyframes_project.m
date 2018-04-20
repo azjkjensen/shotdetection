c025bbedf8e9bed2dec881bcd4f434151d838b23
Jk Jensen <azjkjensen@gmail.com>
2018-04-20 09:54:10 -0700

    Got it working.

--- NEW/ShotDetectionKeyFrameExtraction/extractkeyframes_project.m
+++ NEW/ShotDetectionKeyFrameExtraction/extractkeyframes_project.m
@@ -1,25 +1,32 @@
 function [indices] = extractkeyframes_project(video)
- % create histograms
- %How many bins for the HSV histogram
-h_bins=16;
+ % Number of bins for each component of the HSV histogram
+h_bins=8;
 s_bins=4;
 v_bins=4;
 number_of_bins=h_bins+s_bins+v_bins;
-threshold=.05; %Adjust to get more/less frames
-N=100;  %window size
-frameNumber = 0;
+
+%Adjust to get more/less frames
+threshold=.028; 
+
+%window size
+N=2;  
+frameNumber = N+1;
 a=1;
+
+SAMPLE_SIZE = 500.0;
 
 % read in video
 inputv = VideoReader(video);   % take in inputs
-expectedNumberOfFrames = int32(inputv.duration * inputv.framerate);
+expectedNumberOfFramesExact = inputv.duration * inputv.framerate;
+expectedNumberOfFrames = int32(expectedNumberOfFramesExact);
 totalFramesRead = 0;
+myCount = 1;
 
-for i = 1:int32(expectedNumberOfFrames/500)
-    % Read frames until the end of the video, 500 at a time. 
+for i = 1:int32(ceil(expectedNumberOfFramesExact/SAMPLE_SIZE))
+    % Read frames until the end of the video, SAMPLE_SIZE at a time. 
     framesLeft = expectedNumberOfFrames - totalFramesRead;
-    if framesLeft > 500
-        frames = read(inputv, [totalFramesRead+1 ((totalFramesRead+1)+499)]);
+    if framesLeft > SAMPLE_SIZE
+        frames = read(inputv, [totalFramesRead+1 ((totalFramesRead)+SAMPLE_SIZE)]);
     else
         frames = read(inputv, [(totalFramesRead+1) Inf]);
     end
@@ -34,7 +41,7 @@ for i = 1:int32(expectedNumberOfFrames/500)
         histograms = zeros(expectedNumberOfFrames, number_of_bins);
     end
     
-    frameStart = (500 * (i - 1)) + 1;
+    frameStart = (SAMPLE_SIZE * (i - 1)) + 1;
     for j = frameStart:totalFramesRead
             hsv_image=colorspace('RGB->HSV', frames(:,:,:,j - frameStart + 1));
             h=hsv_image(:,:,1);
@@ -53,6 +60,7 @@ for i = 1:int32(expectedNumberOfFrames/500)
         next_rank=length(find( S/S(1)>threshold));
         
         indices=[];
+        ranks=[];
         possibility=0;
         for t = 3 : numberOfFramesRead-N+1
             rank=next_rank;
@@ -67,17 +75,25 @@ for i = 1:int32(expectedNumberOfFrames/500)
             
             if(rank<next_rank && possibility~=0)
                 indices(a)=possibility;
-                possibility=0;
+                ranks(a)=rank;
                 a=a+1;
             end
-            %sprintf('Frame Number=%d', frameNumber)
             frameNumber=frameNumber+1;
             previous_rank=rank;
         end
-    else %not the first iteration through the while loop
-        for t = 1 : numberOfFramesRead
-            rank=next_rank;     
-            S=svd(histograms(t:t+N-1, :));
+    else %not the first iteration through the loop
+        disp("Frames " + (frameStart) + " to " +  (frameStart + numberOfFramesRead -1));
+        for t = frameStart : frameStart + numberOfFramesRead-1
+%             disp(myCount);
+%             myCount = myCount + 1;
+            rank=next_rank;
+            if (t+N-1) > expectedNumberOfFrames
+                upperLimit = frameStart + numberOfFramesRead - 1;
+            else
+                upperLimit = t + N - 1;
+            end
+            
+            S=svd(histograms(t:upperLimit, :));
             next_rank=length(find( S/S(1)>threshold));
             
             if( (rank>previous_rank))
@@ -87,16 +103,17 @@ for i = 1:int32(expectedNumberOfFrames/500)
             
             if(rank<next_rank && possibility~=0)
                 indices(a)=possibility;
-                possibility=0;
+                ranks(a)=rank;
                 a=a+1;
             end
             frameNumber=frameNumber+1;
+%             sprintf('Frame Number=%d', frameNumber)
             previous_rank=rank;
         end
     end
 end
 
-%disp("Expected number of frames: " + expectedNumberOfFrames + ...
-%    " Actual number of frames: " + numberOfFrames);
+disp("Expected number of frames: " + expectedNumberOfFrames + ...
+   " Actual number of frames: " + totalFramesRead);
 cd('../..');
-end
+end % Returns indices, a vector of frame indices where keyframes are found.
